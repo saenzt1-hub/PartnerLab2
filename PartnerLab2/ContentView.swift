51a50edc18eaad482c03509ae7e1013be4961f39
Taina Saenz <tainamsaenz@gmail.com>
2025-10-01 20:06:13 -0700

    Initial commit

---
+++ PartnerLab2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PartnerLab2
+//
+//  Created by Taina Saenz on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PartnerLab2/PartnerLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerLab2App.swift
+//  PartnerLab2
+//
+//  Created by Taina Saenz on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PartnerLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
