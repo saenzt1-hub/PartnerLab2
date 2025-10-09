a96492ac214aad3cad6884d8b5577cf7ef54bec2
Taina Saenz <tainamsaenz@gmail.com>
2025-10-09 09:18:52 -0700

    Add ContentView layout and CardView structure - half of the app is already finished

--- PartnerLab2/ContentView.swift
+++ PartnerLab2/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+struct Card: Identifiable {
+    let id = UUID()
+    let emoji: String
+    var isFaceUp: Bool = false
+    var isMatched: Bool = false
+}
+
 struct ContentView: View {
+    @State private var cards: [Card] = []
+    @State private var selectedCards: [Int] = []
+    
+    private let flowers = ["flower1", "flower2","flower3", "flower4", "flower5", "flower6", "flower7", "flower8", "flower9","flower10", "flower11", "flower12"]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.3), Color.purple.opacity(0.3)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack {
+                Text("Game")
+                    .font(.system(size: 48, weight: .bold))
+                    .foregroundColor(.blue)
+                    .padding(.top, 20)
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 15) {
+                        ForEach(cards.indices, id: \.self) { index in
+                            CardView(card: cards[index])
+                                .aspectRatio(0.75, contentMode: .fit)
+                            
+                        }
+                    }
+                    .padding()
+                }
+                
+            
+            }
         }
-        .padding()
+    }
+}
+
+struct CardView: View {
+    let card: Card
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.blue, lineWidth: 3)
+                .background(RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.white)
+                )
+            
+        }
     }
 }
 
